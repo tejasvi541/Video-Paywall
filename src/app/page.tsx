74935a5e496f18b8b24b0ae18d06bb3530af9ce3
Tejasvi <tejasvi.tanwar541@gmail.com>
2024-09-09 16:07:31 -0400

    Next Auth step 1 complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,13 @@
-export default function Home() {
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
+
+export default async function Home() {
+  const session = await auth();
+
+  if (!session) {
+    redirect("/api/auth/signin");
+  }
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
--- src/lib/auth.config.ts
+++ src/lib/auth.config.ts
@@ -1,5 +1,9 @@
-import NextAuth from "next-auth";
+import { NextAuthConfig } from "next-auth";
+import GitHub from "next-auth/providers/github";
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
-});
+export default {
+  providers: [GitHub],
+  session: {
+    strategy: "jwt",
+  },
+} satisfies NextAuthConfig;
