5a8bf6e26e0b9f92a7154c4e39e2da90fd86076e
Tejasvi <tejasvi.tanwar541@gmail.com>
2024-09-10 16:03:26 -0400

    Connecting drizzle with authjs and updating the schemas for our database

--- src/lib/auth.config.ts
+++ src/lib/auth.config.ts
@@ -1,7 +1,10 @@
+import { db } from "@/db/db";
 import { NextAuthConfig } from "next-auth";
 import GitHub from "next-auth/providers/github";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
 
 export default {
+  adapter: DrizzleAdapter(db),
   providers: [GitHub],
   session: {
     strategy: "jwt",
