412a0b3aa3f7b3bdf331f7a6e1c5122f79105a74
Tejasvi <tejasvi.tanwar541@gmail.com>
2024-09-15 18:20:54 -0400

    Payments system implemented

--- src/app/(main)/_components/upgrade.tsx
+++ src/app/(main)/_components/upgrade.tsx
@@ -1,12 +1,59 @@
 import { Button } from "@/components/ui/button";
 import { useCreateOrder } from "@/lib/hooks/users/use-create-order";
 import { useVerifyPayment } from "@/lib/hooks/users/user-verify-order";
+import { loadScript } from "@/lib/loadscript";
+import { useEffect } from "react";
 
 const Upgrade = () => {
   const createOrderMutation = useCreateOrder();
   const verifyOrderMutation = useVerifyPayment();
 
-  const onPayment = async () => {};
+  const verifyPayment = async (orderData: any) => {
+    verifyOrderMutation.mutate(
+      {
+        orderId: orderData.razorpay_order_id,
+        paymentId: orderData.razorpay_payment_id,
+        signature: orderData.razorpay_signature,
+      },
+      {
+        onSuccess: async () => {
+          console.log("Payment Success");
+        },
+        onError: async () => {
+          console.log("Payment Failed");
+        },
+      }
+    );
+  };
+
+  const onPayment = async () => {
+    let orderData;
+    createOrderMutation.mutate(
+      {
+        planId: "premium",
+      },
+      {
+        onSuccess: async (data) => {
+          const paymentObject = new (window as any).Razorpay({
+            key: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID,
+            order_id: data.data.id,
+            ...data.data,
+            handler: async function (response: any) {
+              console.log(response);
+
+              orderData = response;
+              await verifyPayment(orderData);
+            },
+          });
+          await paymentObject.open();
+        },
+      }
+    );
+  };
+
+  useEffect(() => {
+    loadScript("https://checkout.razorpay.com/v1/checkout.js");
+  }, []);
 
   return (
     <div>
